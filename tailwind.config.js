fd91884748905daa2e9822c83c5933e9c73e1658
Awais <Awaisali@80.com>
2024-05-10 10:41:28 +0500

    update tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,48 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   // content: [],
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
 /** @type {import('tailwindcss').Config} */
-export default {
-  // content: [],
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+module.exports = {
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            screens: {
+                xxl: "2560px",
+                xl: "1920px",
+                xss: "500px",
+                xsss: "460px",
+                xssss: "350px",
+                md: "650px",
+                // lg: "768px",
+            },
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+                roboto: ["Roboto", "sans-serif"],
+            },
+            colors: {
+                primary: "#1030ff",
+                secondary: "#272727",
+            },
+            width: {
+                inherit: "inherit",
+            },
+        },
+        container: {
+            center: true,
+        },
+    },
+    plugins: [],
 };
